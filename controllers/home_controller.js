d63d79adee778cbf61b372d3f7ce3f89a417679a
Skandesh Suresh <skandeshsuresh@gmail.com>
2021-11-10 19:58:37 +0530

    finished the project !!

---
+++ controllers/home_controller.js
@@ -0,0 +1,56 @@
+const TaskList = require('../models/task');
+
+// controller for home page
+module.exports.home=function(req,res){
+     //fetching data from database
+     TaskList.find({},function(err,task){
+        if(err){
+            console.log('Error in fetching list from db');
+            return;
+        }
+		//render tasks in timeline order
+        return res.render('home',{
+            title : "TaskGuru", 
+            todo_list : task
+         });
+    });
+};
+
+// controller for adding task
+module.exports.add=function(req,res){
+    TaskList.create(req.body,function(err,task){
+        if(err){
+            console.log('error in creating the list');
+            return;
+        }
+        console.log(task);
+        return res.redirect('/');
+    });
+};
+
+// controller for deleting task
+module.exports.delete=function(req,res){
+
+    //getting the id of selected boxes
+    let id = req.query;
+    console.log(id);
+
+    //gives the length of the selected checkboxes
+    let checkboxes=Object.keys(id).length;
+
+    //iterating over each selected boxes
+    for(let i=0;i<checkboxes;i++)
+    {
+        // find the item in the database using id and delete it
+        TaskList.findByIdAndDelete(Object.keys(id)[i],function(err)
+        {
+            if(err)
+            {
+                console.log("error in deleting the item");
+                return;
+            }
+            
+        })
+    }
+    return res.redirect('back');
+};
---
+++ index.js
@@ -0,0 +1,36 @@
+const { static } = require('express');
+
+// required express module
+const express=require('express');
+
+// required db
+const db = require('./config/mongoose');
+
+//selecting the port  
+const port=8000;
+
+//storing express module as a function
+const app=express();
+
+// set up view engine as ejs
+app.set('view engine','ejs');
+app.set('views','./views');
+
+// used static files
+app.use(express.static('./assets'));
+
+// middleware to read url requests
+app.use(express.urlencoded());
+
+// used express routes
+app.use('/',require('./routes/index'));
+
+// app listening on port where server is running
+app.listen(port,function(err){
+    // if error in running the server
+    if(err){
+        console.log('Error Running the Server !');
+        return;
+    }
+    console.log('Server is up and running on port :',port)
+});
